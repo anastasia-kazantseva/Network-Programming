e340b6ad0f7ac2cef26784ada86beed6a5f0871c
Anastasia Kazantseva <kazantseva.an.va@gmail.com>
2016-12-15 17:25:37 +0700

    final version, i hope

--- PortForwarder/Main.java
+++ PortForwarder/Main.java
@@ -11,12 +11,14 @@ class ConnectionInfo {
     private SocketChannel rChannel;
     private ByteBuffer bufferOne;
     private ByteBuffer bufferTwo;
+    private int shutdownState;
 
     ConnectionInfo(SocketChannel sChannel, SocketChannel rChannel) {
         this.sChannel = sChannel;
         this.rChannel = rChannel;
         bufferOne = ByteBuffer.allocate(1024*1024);
         bufferTwo = ByteBuffer.allocate(1024*1024);
+        this.shutdownState = 0;
     }
 
     ByteBuffer getWriteBuffer(SocketChannel channel) {
@@ -29,6 +31,14 @@ class ConnectionInfo {
 
     SocketChannel getChannel(SocketChannel channel) {
         return channel.equals(sChannel) ? rChannel : sChannel;
+    }
+
+    int getShutdownState() {
+        return this.shutdownState;
+    }
+
+    void setShutdownState() {
+        ++this.shutdownState;
     }
 }
 
@@ -60,14 +70,18 @@ public class Main {
             while (keyIterator.hasNext()) {
                 SelectionKey key = keyIterator.next();
 
-                if (key.isAcceptable()) {
-                    acceptKey(channel, selector, rAddress);
-                } else if (key.isConnectable()) {
-                    connectKey(key);
-                } else if (key.isReadable()) {
-                    readKey(key, selector);
-                } else if (key.isWritable()) {
-                    writeKey(key, selector);
+                try {
+                    if (key.isAcceptable()) {
+                        acceptKey(channel, selector, rAddress);
+                    } else if (key.isConnectable()) {
+                        connectKey(key);
+                    } else if (key.isReadable()) {
+                        readKey(key, selector);
+                    } else if (key.isWritable()) {
+                        writeKey(key, selector);
+                    }
+                } catch (CancelledKeyException e) {
+
                 }
 
                 keyIterator.remove();
@@ -76,32 +90,40 @@ public class Main {
     }
 
     private static void acceptKey(ServerSocketChannel channel, Selector selector, SocketAddress rAddress) throws IOException {
-        SocketChannel clientFrom = channel.accept();
-        SocketChannel clientTo = SocketChannel.open();
-
-        clientFrom.configureBlocking(false);
-        clientTo.configureBlocking(false);
-
-        ConnectionInfo info = new ConnectionInfo(clientFrom, clientTo);
-        clientFrom.register(selector, SelectionKey.OP_READ, info);
         try {
-            if (clientTo.connect(rAddress)) {
-                clientTo.register(selector, SelectionKey.OP_READ, info);
-            } else {
-                clientTo.register(selector, SelectionKey.OP_CONNECT, info);
+            SocketChannel clientFrom = channel.accept();
+            SocketChannel clientTo = SocketChannel.open();
+
+            clientFrom.configureBlocking(false);
+            clientTo.configureBlocking(false);
+
+            ConnectionInfo info = new ConnectionInfo(clientFrom, clientTo);
+            clientFrom.register(selector, SelectionKey.OP_READ, info);
+            try {
+                if (clientTo.connect(rAddress)) {
+                    clientTo.register(selector, SelectionKey.OP_READ, info);
+                } else {
+                    clientTo.register(selector, SelectionKey.OP_CONNECT, info);
+                }
+            } catch (IOException e) {
+                clientFrom.keyFor(selector).cancel();
+                clientTo.close();
+                clientFrom.close();
             }
-        } catch (IOException e) {
-            clientFrom.keyFor(selector).cancel();
-            clientTo.close();
-            clientFrom.close();
+        } catch(CancelledKeyException e) {
+
         }
 
         //System.out.println("ACCEPT");
     }
 
     private static void connectKey(SelectionKey key) throws IOException {
-        ((SocketChannel)key.channel()).finishConnect();
-        key.interestOps(key.interestOps() & ~SelectionKey.OP_CONNECT | SelectionKey.OP_READ);
+        try {
+            ((SocketChannel) key.channel()).finishConnect();
+            key.interestOps(key.interestOps() & ~SelectionKey.OP_CONNECT | SelectionKey.OP_READ);
+        } catch (CancelledKeyException e) {
+
+        }
 
         //System.out.println("CONNECT");
     }
@@ -114,16 +136,28 @@ public class Main {
 
             ByteBuffer buffer = info.getReadBuffer(clientFrom);
             try {
-                clientFrom.read(buffer);
+                if (clientFrom.read(buffer) != -1) {
+                    clientTo.keyFor(selector).interestOps(clientTo.keyFor(selector).interestOps() | SelectionKey.OP_WRITE);
+                } else {
+                    clientTo.shutdownOutput();
 
-                clientTo.keyFor(selector).interestOps(clientTo.keyFor(selector).interestOps() | SelectionKey.OP_WRITE);
+                    if (info.getShutdownState() == 1) {
+                        key.cancel();
+                        clientTo.keyFor(selector).cancel();
+                        clientFrom.close();
+                        clientTo.close();
+                    } else {
+                        info.setShutdownState();
+                        clientFrom.keyFor(selector).interestOps(clientFrom.keyFor(selector).interestOps() & ~SelectionKey.OP_READ);
+                    }
+                }
             } catch (IOException e) {
                 key.cancel();
                 clientTo.keyFor(selector).cancel();
                 clientTo.close();
             }
         } catch (CancelledKeyException e) {
-
+            System.out.println("wtf: READ");
         }
         //System.out.println("READ");
     }
@@ -132,23 +166,35 @@ public class Main {
         try {
             ConnectionInfo info = (ConnectionInfo) key.attachment();
             SocketChannel clientTo = (SocketChannel) key.channel();
+            SocketChannel clientFrom = info.getChannel(clientTo);
 
             ByteBuffer buffer = info.getWriteBuffer(clientTo);
             buffer.flip();
             try {
-                clientTo.write(buffer);
-                buffer.compact();
-
-                if (!buffer.hasRemaining()) {
-                    clientTo.keyFor(selector).interestOps(clientTo.keyFor(selector).interestOps() & ~SelectionKey.OP_WRITE);
+                if (clientTo.write(buffer) != -1) {
+                    buffer.compact();
+                    if (!buffer.hasRemaining()) {
+                        clientTo.keyFor(selector).interestOps(clientTo.keyFor(selector).interestOps() & ~SelectionKey.OP_WRITE);
+                    }
+                } else {
+                    clientFrom.shutdownInput();
+                    if (info.getShutdownState() == 1) {
+                        key.cancel();
+                        clientFrom.keyFor(selector).cancel();
+                        clientFrom.close();
+                        clientTo.close();
+                    } else {
+                        info.setShutdownState();
+                        clientFrom.keyFor(selector).interestOps(clientFrom.keyFor(selector).interestOps() & ~SelectionKey.OP_READ);
+                    }
                 }
             } catch (IOException e) {
                 key.cancel();
-                info.getChannel(clientTo).keyFor(selector).cancel();
-                info.getChannel(clientTo).close();
+                clientFrom.keyFor(selector).cancel();
+                clientFrom.close();
             }
         } catch (CancelledKeyException e) {
-
+            System.out.println("wtf: WRITE");
         }
 
         //System.out.println("WRITE");
